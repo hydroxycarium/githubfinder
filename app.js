65b8d6f9c7a2e78a41349a73f96b1e88116233f8
Jeremy Chelot <hello@hydroxycarium.com>
2019-04-30 07:19:21 +0100

    Complete github finder

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const searchUser = document.getElementById('searchUser');
-const gitHub = new GitHub;
+// Init Github
+const github = new Github;
+// Init UI
 const ui = new UI;
 
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-    const userText = e.target.value;
+  // Get input text
+  const userText = e.target.value;
 
-    if(userText != ''){
-        gitHub.getUser(userText)
-        .then(res => {
-            if(res.profile.message === 'Not Found'){
-                console.log('User Not Found');
-            } else {
-                ui.showProfile(res.profile)
-            }
-        })
-        .catch(err => console.log(err))
-    } else {
-
-    }
-})
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
